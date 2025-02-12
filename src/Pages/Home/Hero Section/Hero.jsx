52d2a9ef130c4e427f0e03df06ce6f6478895f79
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-12 08:39:23 +0600

    added text animation on hero section

--- src/Pages/Home/Hero Section/Hero.jsx
+++ src/Pages/Home/Hero Section/Hero.jsx
@@ -8,8 +8,10 @@ const Hero = () => {
           <div className="max-w-screen-lg">
             <h1 className="text-5xl font-bold">
               WelCome to
-              <span className="text-primary"> Code</span>
-              <span className="text-secondary">Flare</span>
+              <span className='bg-gradient-to-r bg-300% from-primary via-blue-500 to-secondary text-transparent bg-clip-text animate-gradient'>
+              <span className=""> Code</span>
+              <span className="">Flare</span>
+              </span>
             </h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
@@ -19,18 +21,18 @@ const Hero = () => {
             <div className="flex gap-x-4 justify-center">
               <Link to={"/blogs"} className="relative inline-block px-4 py-2 font-medium group">
                
-                  <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                  <span class="absolute inset-0 w-full h-full bg-white border-2 border-primary group-hover:bg-primary"></span>
-                  <span class="relative text-primary group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                  <span className="absolute inset-0 w-full h-full bg-white border-2 border-primary group-hover:bg-primary"></span>
+                  <span className="relative text-primary group-hover:text-white">
                     Read blog
                   </span>
                 
               </Link>
 
               <Link to={"/bookmark"} className="relative inline-block px-4 py-2 font-medium group">
-              <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-secondary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                  <span class="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-secondary"></span>
-                  <span class="relative text-secondary group-hover:text-white">
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-secondary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                  <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-secondary"></span>
+                  <span className="relative text-secondary group-hover:text-white">
                     Bookmarks
                   </span>
               </Link>
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from '../Hero Section/Hero'
+import wave from "../../../assets/wave.svg"
 
 
 const Home = () => {
   return (
-    <div className='flex items-center justify-center min-h-[calc(100vh-116px)]'>
+    <div className='flex flex-col items-center justify-center relative min-h-[calc(100vh-116px)]'>
       <Hero/>
+      <img className='absolute bottom-0 w-full' src={wave} alt="" />
     </div>
   )
 }
