c77966089305632fed9fc471fba413cb870cb222
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-16 07:19:23 +0600

    added delete button on bookmark

--- src/Pages/Blogs/BlogsCard.jsx
+++ src/Pages/Blogs/BlogsCard.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import { Link } from "react-router";
 import placeholderImage from "../../assets/404.jpg";
+import { RxCrossCircled } from "react-icons/rx";
 
-const BlogsCard = ({ blog }) => {
+const BlogsCard = ({ blog, deleteable }) => {
   const { title, cover_image, published_at, description, id } = blog;
   return (
-    <>
-      <Link
-        to={`/blogs/${id}`}
-        className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 border-2 transition hover:scale-105 border-primary hover:border-secondary rounded-sm"
-      >
-        <img
-          role="presentation"
-          className="object-cover w-full rounded h-44 dark:bg-gray-500"
-          src={cover_image || placeholderImage}
-        />
-        <div className="p-6 space-y-2">
-          <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
-            {title}
-          </h3>
-          <span className="text-xs dark:text-gray-600">
-            {new Date(published_at).toLocaleDateString()}
-          </span>
-          <p>{description}</p>
+    
+      <div className='relative max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 border-2 transition hover:scale-105 border-primary hover:border-secondary rounded-sm'>
+        <Link
+          to={`/blogs/${id}`}
+          className=""
+        >
+          <img
+            role="presentation"
+            className="object-cover w-full rounded h-44 dark:bg-gray-500"
+            src={cover_image || placeholderImage}
+          />
+          <div className="p-6 space-y-2">
+            <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+              {title}
+            </h3>
+            <span className="text-xs dark:text-gray-600">
+              {new Date(published_at).toLocaleDateString()}
+            </span>
+            <p>{description}</p>
+          </div>
+        </Link>
+      {deleteable && (
+      <div onClick={()=>handleDelete} className="bg-primary absolute w-6 h-6 rounded-full flex justify-center items-center cursor-pointer top-0 right-0">
+        <RxCrossCircled className="text-secondary" />
         </div>
-      </Link>
-    </>
+      )}
+      </div>
+    
   );
 };
 
--- src/Pages/BookMark/BookMark.jsx
+++ src/Pages/BookMark/BookMark.jsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router'
+import { getBlogLS } from '../../Utility/getblogLS'
+import Loader from '../../components/Loader'
+import BlogsCard from '../Blogs/BlogsCard'
 
 
 const BookMark = () => {
-  const data = useLoaderData()
-  console.log(data)
+ const [bookmarkData, setBookMarkData]= useState(null)
+ useEffect(()=>{
+  const data = getBlogLS()
+  setBookMarkData(data)
+ },[])
+ if(!bookmarkData){
+  return <Loader/>
+ }
+ console.log(bookmarkData)
   return (
-    <div>
-      <h1>book mark</h1>
-    </div>
+    <div className="grid justify-center px-12 py-8 grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    {
+      bookmarkData.map((blog, index)=>(
+        <BlogsCard
+          key={index}
+          blog={blog}
+          deleteable = {true}
+        />
+      ))
+    }
+  </div>
   )
 }
 
